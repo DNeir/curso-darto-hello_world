4cfd0b08ab02032e447fa1e9d48809b8a351aeff
Neir <neirdaza@gmail.com>
2024-03-27 14:15:24 -0500

    Agregar libreria Characters para manejar emojis

--- bin/hello_word.dart
+++ bin/hello_word.dart
@@ -1,5 +1,8 @@
-import 'package:hello_word/hello_word.dart' as hello_word;
+//import 'package:hello_word/hello_word.dart' as hello_word;
+import 'package:characters/characters.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${hello_word.calculate()}!');
+  //print('Hello world: ${hello_word.calculate()}!');
+  const texto = 'Hola 👍';
+  print(texto.characters.length);
 }
